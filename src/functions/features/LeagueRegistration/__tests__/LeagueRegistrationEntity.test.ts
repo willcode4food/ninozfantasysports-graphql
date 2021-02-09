6532102b2353a22cc6afa3764fa72acac0a6acc3
Marc Arbesman <marbesman@gmail.com>
2021-02-08 21:27:19 -0500

    adds all tests for league registration

--- src/functions/features/LeagueRegistration/__tests__/LeagueRegistrationEntity.test.ts
+++ src/functions/features/LeagueRegistration/__tests__/LeagueRegistrationEntity.test.ts
@@ -3,6 +3,8 @@ import { initialize } from 'fireorm/lib/src/MetadataStorage'
 import { ID, Int } from 'type-graphql'
 
 const MockFirebase = require('mock-cloud-firestore')
+const expectedUserId = 'AUsewNSXhRJuoKZoqiqdgIDWHp2'
+
 const fixtureData = {
     __collection__: {
         leagueRegistration: {
@@ -11,9 +13,65 @@ const fixtureData = {
                     dateRegistered: new Date('2021-01-17T03:24:00'),
                     id: 'DqEhYylTEbHiGNkRflaP',
                     isActive: true,
-                    leagueId: 'sEREsBNeOM16BtRaSbzv',
-                    seasonId: 'DDqVvYGHKX4ayl97qrfy',
-                    userId: 'vhPn3q0IqaNSZwWFEHAykEsEVYd2',
+                    leagueId: 'BQzNm7DPdpInAy6sRCxL',
+                    seasonId: 'AAqVvYGHKX4ayl97qrfy',
+                    userId: expectedUserId,
+                },
+            },
+        },
+        users: {
+            __doc__: {
+                [expectedUserId]: {
+                    id: expectedUserId,
+                    firstName: 'Homer',
+                    lastName: 'Simpson',
+                    username: 'hsimpson',
+                    email: 'hsimpson@springfieldpower.com',
+                    loginProvider: 'email',
+                    defaultAvatarThemeIndex: 0,
+                },
+                QUsewNSXhRJuoKZoqiqdgIDWHp2: {
+                    id: 'QUsewNSXhRJuoKZoqiqdgIDWHp2',
+                    firstName: 'Marge',
+                    lastName: 'Simpson',
+                    username: 'msimpson',
+                    email: 'msimpson@springfield.net',
+                    loginProvider: 'google',
+                    defaultAvatarThemeIndex: 0,
+                },
+            },
+        },
+        seasons: {
+            __doc__: {
+                AAqVvYGHKX4ayl97qrfy: {
+                    id: 'AAqVvYGHKX4ayl97qrfy',
+                    leagueId: 'BQzNm7DPdpInAy6sRCxL',
+                    name: 'Season 1',
+                    startDate: new Date(2019, 10, 30).toISOString(),
+                    endDate: new Date(2019, 11, 30).toISOString(),
+                },
+                JJqVvYGHKX4ayl97qrfy: {
+                    id: 'JJqVvYGHKX4ayl97qrfy',
+                    leagueId: 'DDqVvYGHKX4ayl97qrfy',
+                    name: 'Season 2',
+                    startDate: new Date(2018, 10, 30).toISOString(),
+                    endDate: new Date(2018, 11, 30).toISOString(),
+                },
+            },
+        },
+        leagues: {
+            __doc__: {
+                BQzNm7DPdpInAy6sRCxL: {
+                    id: 'BQzNm7DPdpInAy6sRCxL',
+                    name: 'Conflict of Enemies 2021',
+                    leagueId: '123Nm7DPdpInAy6sRCxL',
+                    type: 'esports',
+                },
+                DDqVvYGHKX4ayl97qrfy: {
+                    id: 'DDqVvYGHKX4ayl97qrfy',
+                    name: 'Conflict of Enemies 2020',
+                    leagueId: '123Nm7DPdpInAy6sRCxL',
+                    type: 'esports',
                 },
             },
         },
@@ -56,6 +114,31 @@ const getRegistrationByLeague = `
     }
 `
 
+const createLeagueRegistration = `
+    mutation CreateLeagueRegistration($data: LeagueRegistrationInput!){
+        createLeagueRegistration(data: $data) {
+            userId,
+            seasonId,
+            leagueId,
+            id,
+            dateRegistered,
+            isActive
+        }
+}
+`
+
+const updateLeagueRegistration = `
+    mutation UpdateLeagueRegistration($data: LeagueRegistrationUpdateInput!){
+        updateLeagueRegistration(data: $data) {
+            leagueId
+            seasonId
+            userId
+            dateRegistered
+            isActive
+        }
+    }
+`
+
 describe('League Registration Entity Repository', () => {
     beforeEach(() => {
         const firebase = new MockFirebase(fixtureData)
@@ -72,7 +155,7 @@ describe('League Registration Entity Repository', () => {
         const response = await graphqlCall({
             source: getRegistrationByUser,
             variableValues: {
-                userId: 'vhPn3q0IqaNSZwWFEHAykEsEVYd2',
+                userId: expectedUserId,
             },
         })
         expect(response.data.returnLeagueRegistrationByUser.length).toBe(1)
@@ -82,7 +165,7 @@ describe('League Registration Entity Repository', () => {
         const response = await graphqlCall({
             source: getRegistrationBySeason,
             variableValues: {
-                seasonId: 'DDqVvYGHKX4ayl97qrfy',
+                seasonId: 'AAqVvYGHKX4ayl97qrfy',
             },
         })
         expect(response.data.returnLeagueRegistrationBySeason.length).toBe(1)
@@ -93,9 +176,57 @@ describe('League Registration Entity Repository', () => {
         const response = await graphqlCall({
             source: getRegistrationByLeague,
             variableValues: {
-                leagueId: 'sEREsBNeOM16BtRaSbzv',
+                leagueId: 'BQzNm7DPdpInAy6sRCxL',
             },
         })
         expect(response.data.returnLeagueRegistrationByLeague.length).toBe(1)
+    })
+
+    it('is a resolver of type LeagueRegistration', () => {
+        const { resolveLeagueRegistration } = require('../LeagueRegistrationResolver')
+        const { LeagueRegistration } = require('../LeagueRegistrationEntity')
+
+        expect(resolveLeagueRegistration()).toBe(LeagueRegistration)
+    })
+    it('creates a league registration', async () => {
+        const { graphqlCall } = require('../../../../test-utils/graphqlCall')
+        const leagueRegistration = {
+            leagueId: 'DDqVvYGHKX4ayl97qrfy',
+            seasonId: 'JJqVvYGHKX4ayl97qrfy',
+            userId: 'QUsewNSXhRJuoKZoqiqdgIDWHp2',
+        }
+        const response = await graphqlCall({
+            source: createLeagueRegistration,
+            variableValues: {
+                data: leagueRegistration,
+            },
+        })
+        expect(response.data.createLeagueRegistration).toMatchObject({
+            isActive: true,
+            leagueId: leagueRegistration.leagueId,
+            seasonId: leagueRegistration.seasonId,
+            userId: leagueRegistration.userId,
+        })
+    })
+    it('sets a registration to inactive', async () => {
+        // updateLeagueRegistration
+        const { graphqlCall } = require('../../../../test-utils/graphqlCall')
+        const leagueRegistrationUpdate = {
+            id: 'DqEhYylTEbHiGNkRflaP',
+            isActive: false,
+        }
+        const response = await graphqlCall({
+            source: updateLeagueRegistration,
+            variableValues: {
+                data: leagueRegistrationUpdate,
+            },
+        })
+        expect(response.data.updateLeagueRegistration).toMatchObject({
+            dateRegistered: new Date('2021-01-17T03:24:00').toISOString(),
+            isActive: false,
+            leagueId: 'BQzNm7DPdpInAy6sRCxL',
+            seasonId: 'AAqVvYGHKX4ayl97qrfy',
+            userId: expectedUserId,
+        })
     })
 })
